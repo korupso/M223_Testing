05c7d06e92e9425f7aab64f37dfa1ce5c11d133f
korupso <joel.meccariello@gmail.com>
2020-09-09 16:43:05 +0200

    Travis test

--- src/test/java/ch/noseryoung/uk/domainModels/auction/unitTests/AuctionRepositoryTest.java
+++ src/test/java/ch/noseryoung/uk/domainModels/auction/unitTests/AuctionRepositoryTest.java
@@ -1,24 +1,20 @@
 package ch.noseryoung.uk.domainModels.auction.unitTests;
 
-import org.assertj.core.api.Assertions;
+import java.util.Optional;
 
-import ch.noseryoung.uk.domainModels.auction.AuctionRepository;
-import ch.noseryoung.uk.domainModels.auction.Auction;
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Optional;
-
-import static org.mockito.Mockito.verify;
+import ch.noseryoung.uk.domainModels.auction.Auction;
+import ch.noseryoung.uk.domainModels.auction.AuctionRepository;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
